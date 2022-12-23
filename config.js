e4e8697272c12eabf2cc4d681faedfcbb3baadaf
Sangeetha-Naresh <sangeetha.nareshm@gmail.com>
2022-12-23 16:17:19 +0530

    Added SDK

--- config.js
+++ config.js
@@ -1,9 +1,20 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
 
+
+    const firebaseConfig = {
+ apiKey: "AIzaSyDn8_R2DlL-7l1kn-rv76SInCqdqs8MNW0",
+  authDomain: "project58-f31e2.firebaseapp.com",
+  databaseURL: "https://project58-f31e2-default-rtdb.firebaseio.com",
+  projectId: "project58-f31e2",
+  storageBucket: "project58-f31e2.appspot.com",
+  messagingSenderId: "511154109183",
+  appId: "1:511154109183:web:0a735b01c12dfea98f6aca"
 };
+
+
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
